08e71c06facc1bf5176a6812dd9c37ed1d2c5024
KaserChan <chankaser@gmail.com>
2020-08-31 15:58:49 +0800

    feat: add http server for test

--- cmd/test/http_server.go
+++ cmd/test/http_server.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+
+	_ "github.com/BoynChan/GopherProxy/pkg"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
+
 // Start a simple real http server.
 // Author:Boyn
 // Date:2020/8/31
+
+func newGinServer(addr string) *gin.Engine {
+	r := gin.Default()
+	r.GET("/*path", func(c *gin.Context) {
+		// in this handler, we just simply send some basic info back to proxy response.
+		req := c.Request
+		path := c.Param("path")
+		fmt.Println(path)
+		urlPath := fmt.Sprintf("http://%s%s", addr, req.RequestURI)
+		realIP := fmt.Sprintf("RemoteAddr=%s,X-Forwarded-For=%v,X-Real-Ip=%v", req.RemoteAddr, req.Header.Get("X-Forwarded-For"), req.Header.Get("X-Real-Ip"))
+		c.JSON(200, gin.H{
+			"path": urlPath,
+			"ip":   realIP,
+		})
+	})
+	return r
+}
+
+func main() {
+	wg := sync.WaitGroup{}
+	addrs := viper.GetStringSlice("Http.RealServer")
+	wg.Add(len(addrs))
+	for _, a := range addrs {
+		go func(addr string) {
+			if err := newGinServer(addr).Run(addr); err != nil {
+				wg.Done()
+			}
+		}(a)
+	}
+	wg.Wait()
+}
