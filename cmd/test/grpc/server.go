3fda4f769d98cde6f7b6bb6053e22c697ea85718
KaserChan <chankaser@gmail.com>
2020-09-04 15:59:13 +0800

    feat: add load balance in grpc proxy server

--- cmd/test/grpc/server.go
+++ cmd/test/grpc/server.go
@@ -2,25 +2,38 @@ package main
 
 import (
 	"fmt"
-	pb "github.com/BoynChan/GopherProxy/internal/grpc/proto"
-	"github.com/BoynChan/GopherProxy/internal/grpc/server"
-	"google.golang.org/grpc"
 	"log"
 	"net"
+	"sync"
+
+	pb "github.com/BoynChan/GopherProxy/internal/grpc/proto"
+	"github.com/BoynChan/GopherProxy/internal/grpc/server"
+	_ "github.com/BoynChan/GopherProxy/pkg"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 // Author:Boyn
 // Date:2020/9/2
 
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50055))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+	addrs := viper.GetStringSlice("Grpc.RealServer")
+	wg := sync.WaitGroup{}
+	wg.Add(len(addrs))
+	for _, addr := range addrs {
+		go func(address string) {
+			lis, err := net.Listen("tcp", address)
+			if err != nil {
+				log.Fatalf("failed to listen: %v", err)
+			}
+			fmt.Printf("server listening at %v\n", lis.Addr())
+			s := grpc.NewServer()
+			pb.RegisterEchoServer(s, &server.EchoServerImpl{})
+			if err := s.Serve(lis); err != nil {
+				panic(err)
+			}
+			wg.Done()
+		}(addr)
 	}
-	fmt.Printf("server listening at %v\n", lis.Addr())
-	s := grpc.NewServer()
-	pb.RegisterEchoServer(s, &server.EchoServerImpl{})
-	if err := s.Serve(lis); err != nil {
-		panic(err)
-	}
+	wg.Wait()
 }
---
+++ internal/proxy/grpc_proxy.go
@@ -0,0 +1,46 @@
+package proxy
+
+import (
+	"context"
+	"strings"
+
+	"github.com/BoynChan/GopherProxy/internal/loadbalance"
+	"github.com/BoynChan/GopherProxy/internal/urls"
+	proxy "github.com/e421083458/grpc-proxy/proxy"
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+// Author:Boyn
+// Date:2020/9/3
+
+func NewGrpcProxyServer(urlSli []string, lbType loadbalance.Type) (*grpc.Server, error) {
+	registerAddr := viper.GetString("ZookeeperAddr")
+	dyUrls, err := urls.NewDynamicUrls(urlSli, lbType, registerAddr)
+	if err != nil {
+		return nil, err
+	}
+	director := func(ctx context.Context, fullMethodName string) (context.Context, *grpc.ClientConn, error) {
+		nextAddr, err := dyUrls.GetNext("")
+		if err != nil {
+			return ctx, nil, err
+		}
+		// 拒绝某些特殊请求
+		if strings.HasPrefix(fullMethodName, "/com.example.internal.") {
+			return ctx, nil, status.Errorf(codes.Unimplemented,
+				"Unknown method")
+		}
+		c, err := grpc.DialContext(ctx, nextAddr, grpc.WithCodec(proxy.Codec()), grpc.WithInsecure())
+		md, _ := metadata.FromIncomingContext(ctx)
+		outCtx, _ := context.WithCancel(ctx)
+		outCtx = metadata.NewOutgoingContext(outCtx, md.Copy())
+		return outCtx, c, err
+	}
+
+	return grpc.NewServer(
+		grpc.CustomCodec(proxy.Codec()),
+		grpc.UnknownServiceHandler(proxy.TransparentHandler(director))), nil
+}
