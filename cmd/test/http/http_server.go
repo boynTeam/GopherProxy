7ad4c230f2e6fd2be2996a3ba0c0913dab7c68d5
KaserChan <chankaser@gmail.com>
2020-09-07 09:32:15 +0800

    update: add service name in http real server

--- cmd/test/http/http_server.go
+++ cmd/test/http/http_server.go
@@ -25,24 +25,27 @@ const (
 )
 
 type HttpServer struct {
-	Addr     string
-	r        *gin.Engine
-	certFile string
-	keyFile  string
+	Addr        string
+	ServiceName string
+	r           *gin.Engine
+	certFile    string
+	keyFile     string
 }
 
-func NewHttpServer(addr string, file ...string) *HttpServer {
+func NewHttpServer(addr, serviceName string, file ...string) *HttpServer {
 	if len(file) == 2 {
 		return &HttpServer{
-			r:        gin.Default(),
-			Addr:     addr,
-			certFile: file[0],
-			keyFile:  file[1],
+			r:           gin.Default(),
+			Addr:        addr,
+			certFile:    file[0],
+			keyFile:     file[1],
+			ServiceName: serviceName,
 		}
 	}
 	return &HttpServer{
-		r:    gin.Default(),
-		Addr: addr,
+		r:           gin.Default(),
+		Addr:        addr,
+		ServiceName: serviceName,
 	}
 }
 
@@ -53,7 +56,7 @@ func (h *HttpServer) Post(path string, f ...gin.HandlerFunc) {
 	h.r.POST(path, f...)
 }
 
-func (h *HttpServer) Run() error {
+func (h *HttpServer) register() error {
 	zkHostIp := os.Getenv("ZK_HOST_IP")
 	if zkHostIp == "" {
 		return errors.New("no zookeeper running")
@@ -61,40 +64,44 @@ func (h *HttpServer) Run() error {
 	go func() {
 		manager := register.NewZkManager(zkHostIp)
 		_ = manager.GetConnect()
-		err := manager.RegistServerTmpNode(zkRegisterPrefix, h.Addr, []byte(fmt.Sprintf("http://%s", h.Addr))...)
+		servicePrefix := fmt.Sprintf("%s/%s", zkRegisterPrefix, h.ServiceName)
+		err := manager.RegistServerTmpNode(servicePrefix, h.Addr, []byte(fmt.Sprintf("http://%s", h.Addr))...)
 		if err != nil {
 			logrus.Errorf("Register error: %v  addr: %s", err, h.Addr)
 		}
 	}()
+	return nil
+}
+
+func (h *HttpServer) Run() error {
+	err := h.register()
+	if err != nil {
+		return err
+	}
 	return h.r.Run(h.Addr)
 }
 
 func (h *HttpServer) RunTLS() error {
-	zkHostIp := os.Getenv("ZK_HOST_IP")
-	if zkHostIp == "" {
-		return errors.New("no zookeeper running")
+	err := h.register()
+	if err != nil {
+		return err
 	}
-	go func() {
-		manager := register.NewZkManager(zkHostIp)
-		_ = manager.GetConnect()
-		err := manager.RegistServerTmpNode(zkRegisterPrefix, h.Addr, []byte(fmt.Sprintf("https://%s", h.Addr))...)
-		if err != nil {
-			logrus.Errorf("Register error: %v  addr: %s", err, h.Addr)
-		}
-	}()
 	return h.r.RunTLS(h.Addr, h.certFile, h.keyFile)
 }
 
-func newGinServer(addr string, file ...string) *HttpServer {
-	r := NewHttpServer(addr, file...)
+func newGinServer(addr, serviceName string, file ...string) *HttpServer {
+	r := NewHttpServer(addr, serviceName, file...)
 	r.Get("/*path", func(c *gin.Context) {
 		// in this handler, we just simply send some basic info back to proxy response.
 		req := c.Request
 		urlPath := fmt.Sprintf("https://%s%s", addr, req.RequestURI)
-		realIP := fmt.Sprintf("RemoteAddr=%s,X-Forwarded-For=%v,X-Real-Ip=%v", req.RemoteAddr, req.Header.Get("X-Forwarded-For"), req.Header.Get("X-Real-Ip"))
+		retData := make(map[string]interface{})
+		retData["RemoteAddr"] = req.RemoteAddr
+		retData["X-Forwarded-For"] = req.Header.Get("X-Forwarded-For")
+		retData["X-Real-Ip"] = req.Header.Get("X-Real-Ip")
 		c.JSON(http.StatusOK, pkg.NewMessageBuilder().Data(gin.H{
 			"path": urlPath,
-			"ip":   realIP,
+			"ip":   retData,
 		}).Build())
 	})
 	return r
@@ -106,11 +113,12 @@ func main() {
 	addrs := viper.GetStringSlice("Http.RealServer")
 	certFile := viper.GetString("Http.Cert")
 	keyFile := viper.GetString("Http.Key")
+	serviceName := "TEST_HTTP_PROXY"
 	wg.Add(len(addrs))
 	for _, a := range addrs {
 		go func(addr string) {
 			hostPortAddr := strings.Split(addr, "/")[2]
-			if err := newGinServer(hostPortAddr, certFile, keyFile).RunTLS(); err != nil {
+			if err := newGinServer(hostPortAddr, serviceName, certFile, keyFile).RunTLS(); err != nil {
 				logrus.Errorf("Run error %v", err)
 				wg.Done()
 			}
