0fcd57f6cad4c377a474412b393d0019fcc30619
KaserChan <chankaser@gmail.com>
2020-09-08 16:04:05 +0800

    feat: add router init function

--- router/router.go
+++ router/router.go
@@ -1,10 +1,18 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"sync"
+
+	"github.com/BoynChan/GopherProxy/router/user"
+	"github.com/gin-gonic/gin"
+)
 
 // Author:Boyn
 // Date:2020/9/8
+var once sync.Once
 
 func InitRouter(r *gin.Engine) {
-
+	once.Do(func() {
+		user.InitUserRouter(r)
+	})
 }
